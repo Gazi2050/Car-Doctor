3f66571cbeeb689e7c91b5b5fcfbf554eb79416d
Gazi2050 <gazi45344@gmail.com>
2023-10-24 14:02:09 +0600

    navbar,home,footer added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <p className="text-center text-4xl font-bold">This is home</p>
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
 import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import router from './Route/Route';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div className='text-center font-bold text-2xl m-20'>Hello world!</div>,
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
