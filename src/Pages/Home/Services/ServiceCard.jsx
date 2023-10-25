97812a612c517402a0cb25e21998661aac4b5ad2
Gazi2050 <gazi45344@gmail.com>
2023-10-25 16:28:08 +0600

    contact added

---
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,23 @@
+
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+const ServiceCard = ({ service }) => {
+    const { title, img, price } = service;
+    return (
+        <div>
+            <div className="card w-full h-full bg-white border-2 rounded-lg ">
+                <figure className="p-6">
+                    <img src={img} alt="Shoes" className="rounded-lg" />
+                </figure>
+                <div className="card-body ">
+                    <h2 className="card-title">{title}</h2>
+                    <p className="text-xl text-[#FF3811] font-semibold">Price: {price}</p>
+                    <div className="card-actions  ml-auto ">
+                        <BsFillArrowRightCircleFill className="text-[#FF3811] text-3xl hover:text-black" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,9 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 
 const Services = () => {
-    return (
-        <div>
+    const [services, setServices] = useState([]);
 
+    useEffect(() => {
+        fetch('Services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+
+    return (
+        <div className="mt-4 mb-4 space-y-4">
+            <div className="text-center space-y-4">
+                <h3 className='text-[#FF3811] font-bold'>Service</h3>
+                <h1 className="text-5xl font-bold">Our Service Area</h1>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomized <br /> words which do not look even slightly believable. </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
+                {
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                }
+            </div>
+            <div className="flex justify-center items-center">
+                <button className='btn btn-outline text-[#FF3811]'>More Services</button>
+            </div>
         </div>
     );
 };
